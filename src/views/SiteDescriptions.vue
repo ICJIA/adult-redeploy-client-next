20d5fa306e6344240341c2455555f4094b579bfc
cschweda <cschweda@gmail.com>
2019-08-05 11:23:54 -0500

    fix: add site descriptions

--- src/views/SiteDescriptions.vue
+++ src/views/SiteDescriptions.vue
@@ -5,41 +5,38 @@
         <v-container v-if="content">
           <v-layout wrap>
             <v-flex xs12>
-              <h1 class="page-title">Site Descriptions</h1>
+              <h1 class="page-title">{{ content[0].title }}</h1>
             </v-flex>
           </v-layout>
         </v-container>
       </template>
-      <template slot="site-list" v-if="content">
-        <base-list :items="content">
+      <template v-slot:content>
+        <v-container v-if="content">
+          <v-layout wrap v-if="content[0].summary">
+            <v-flex :[dynamicFlex]="true">
+              <div v-html="renderToHtml(content[0].summary)"></div>
+            </v-flex>
+            <v-flex xs2 v-if="showToc"><TOC></TOC></v-flex>
+          </v-layout>
+        </v-container>
+      </template>
+      <template slot="site-list" v-if="sites">
+        <base-list :items="sites">
           <template slot-scope="item">
             <v-container>
               <v-flex xs12>
-                <v-hover v-slot:default="{ hover }">
-                  <v-card
-                    class="mx-auto"
-                    color="white"
-                    v-if="content"
-                    :elevation="hover ? 18 : 2"
-                    @click="toggleDisplay(item.slug)"
-                  >
-                    <v-card-title class="site-desription-title px-3">{{
-                      item.title
-                    }}</v-card-title>
-                    <v-card-text>
-                      <div
-                        v-html="displayContent(item)"
-                        class="markdown-body"
-                      ></div>
-                    </v-card-text>
-                    <v-card-actions
-                      @click.stop="toggleDisplay(item.slug)"
-                      class="hover"
-                    >
-                      {{ toggleMoreLess(item.slug) }}
-                    </v-card-actions>
-                  </v-card>
-                </v-hover>
+                <v-card class="mx-auto" color="white" v-if="sites">
+                  <v-card-title class="site-desription-title px-3">{{
+                    item.title
+                  }}</v-card-title>
+                  <v-card-text>
+                    <div
+                      v-html="renderToHtml(item.content)"
+                      class="markdown-body"
+                    ></div>
+                  </v-card-text>
+                  <v-card-actions class="hover py-3 px-3"> </v-card-actions>
+                </v-card>
               </v-flex>
             </v-container>
           </template>
@@ -53,7 +50,7 @@
 import BaseContent from "@/components/BaseContent";
 import BaseList from "@/components/BaseList";
 
-import { getAllSiteDescriptions } from "@/services/Content";
+import { getAllSiteDescriptions, getPageBySection } from "@/services/Content";
 import { getHash, checkIfValidPage } from "@/services/Utilities";
 import { renderToHtml } from "@/services/Markdown";
 export default {
@@ -68,12 +65,13 @@ export default {
       renderToHtml,
       showToc: false,
       sectionContent: null,
-      descriptionDisplay: []
+      descriptionDisplay: [],
+      expand: false,
+      sites: null
     };
   },
   components: {
     BaseContent,
-
     BaseList
   },
   created() {
@@ -90,60 +88,39 @@ export default {
       this.loading = true;
 
       const contentMap = new Map();
+      const section = "sites";
+      const slug = "sites";
 
-      const name = `getAllSiteDescriptions`;
-      contentMap.set(name, {
-        hash: getHash(name),
+      const getPageName = `getPageBySection-${section}${slug}`;
+
+      contentMap.set(getPageName, {
+        hash: getHash(getPageName),
+        query: getPageBySection,
+        params: { section, slug }
+      });
+
+      const getSitesName = "getAllSiteDescriptions";
+
+      contentMap.set(getSitesName, {
+        hash: getHash(getSitesName),
         query: getAllSiteDescriptions,
         params: {}
       });
 
       await this.$store.dispatch("cacheContent", contentMap);
 
-      this.content = this.$store.getters.getContentFromCache(contentMap, name);
-
-      let descriptionDisplay = this.content.map(site => {
-        let obj = {};
-        obj.slug = site.slug;
-        obj.showSummary = true;
-        return obj;
-      });
-
-      this.descriptionDisplay = descriptionDisplay;
+      this.content = this.$store.getters.getContentFromCache(
+        contentMap,
+        getPageName
+      );
+      this.sites = this.$store.getters.getContentFromCache(
+        contentMap,
+        getSitesName
+      );
 
       this.loading = false;
     },
-    toggleDisplay(slug) {
-      this.descriptionDisplay.filter(item => {
-        if (item.slug === slug) {
-          return (item.showSummary = !item.showSummary);
-        }
-      });
-    },
-    toggleMoreLess(slug) {
-      let display = this.descriptionDisplay.filter(item => {
-        if (item.slug === slug) {
-          return item;
-        }
-      });
-      if (display[0].showSummary) {
-        return "More";
-      } else {
-        return "Less";
-      }
-    },
-    displayContent(site) {
-      const display = this.descriptionDisplay.filter(item => {
-        if (item.slug === site.slug) {
-          return item;
-        }
-      });
-      if (display[0].showSummary) {
-        return site.summary;
-      } else {
-        return this.renderToHtml(site.content);
-      }
-    },
+
     routeToError() {
       this.content = null;
       this.loading = false;
