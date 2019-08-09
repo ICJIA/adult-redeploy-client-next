903cdd58a409abc4c2d2c1d1b4e8f5e5aeedb4c5
cschweda <cschweda@gmail.com>
2019-08-09 11:34:31 -0500

    fix: adjust meeting layout

--- src/components/MeetingCard.vue
+++ src/components/MeetingCard.vue
@@ -12,7 +12,36 @@
         v-if="content.content"
         class="site-description"
       ></div>
-      <v-container>
+
+      <div style="background: #eee" class="px-6 py-2">
+        <h3 style="color: #222;" class="mt-10">Meeting Materials</h3>
+        <ul style="color: #222;" class="mt-4 mb-12">
+          <div v-for="(file, index) in content.materials" :key="index">
+            <li>
+              <span @click="downloadFile(file)" class="hover medium">
+                {{ file.name }}</span
+              >
+              &nbsp;
+              <span
+                style="font-size: 12px; "
+                class="hover onClickLink"
+                @click="downloadFile(file)"
+                >[ Primary download</span
+              >
+              |
+              <span style="font-size: 12px"
+                ><a
+                  :href="`${$store.getters.config.baseURL}/${file.url}`"
+                  target="_blank"
+                  class="hover "
+                  >Alternate download ]</a
+                ></span
+              >
+            </li>
+          </div>
+        </ul>
+      </div>
+      <v-container class="mt-4">
         <v-layout>
           <v-flex xs12 sm12 md6>
             <div class="text-left" v-if="content.createdAt">
@@ -35,12 +64,22 @@
 </template>
 
 <script>
+import { getFile } from "@/services/Download";
 import { renderToHtml } from "@/services/Markdown";
 export default {
   data() {
     return {
       renderToHtml
     };
+  },
+  methods: {
+    downloadFile(item) {
+      // if (item.file) {
+      //   return getFile(item.file);
+      // }
+      console.log(item);
+      return getFile(item);
+    }
   },
   props: {
     content: {
@@ -50,3 +89,15 @@ export default {
   }
 };
 </script>
+
+<style>
+.onClickLink {
+  text-decoration: underline;
+  color: #065f60;
+  font-weight: bold;
+}
+
+.onClickLink:hover {
+  color: #aaa;
+}
+</style>
