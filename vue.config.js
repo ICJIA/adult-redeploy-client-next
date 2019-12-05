d913d3806debec3329866a74e592a02a872f9119
cschweda <cschweda@gmail.com>
2019-12-05 11:02:51 -0600

    fix: restore public path in vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
+const config = require("./src/config.json");
+
 module.exports = {
+  publicPath: process.env.NODE_ENV === `production` ? config.publicPath : "/",
   transpileDependencies: ["vuetify"],
   pluginOptions: {
     moment: {
